ae55d5c3e565e5bb32d7a2e96b3459bb545ee5a9
Mark-Y <yakushyn.mark@gmail.com>
2021-08-21 02:29:48 +0300

    закончил задание

---
+++ index.js
@@ -0,0 +1,46 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const refs = {
+  startButtonRef: document.querySelector("[data-action=start]"),
+
+  stopButtonRef: document.querySelector("[data-action=stop]"),
+
+  bodyRef: document.querySelector("body"),
+};
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let timerId = null;
+
+refs.startButtonRef.addEventListener("click", () => {
+  console.log("start");
+
+  refs.startButtonRef.disabled = true;
+
+  timerId = setInterval(() => {
+    console.log(
+      (refs.bodyRef.style.backgroundColor =
+        colors[randomIntegerFromInterval(0, colors.length - 1)])
+    );
+  }, 1000);
+});
+
+refs.stopButtonRef.addEventListener("click", () => {
+  if (refs.startButtonRef.disabled !== true) {
+    return;
+  }
+
+  clearInterval(timerId);
+  refs.startButtonRef.disabled = false;
+
+  console.log("stop");
+});
